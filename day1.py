5ce93284c44fd3bc5f91fa74a1d0f464672afe6b
Shra-creator <shindeshraddha232@gmail.com>
2026-06-16 19:02:21 +0530

    Todays work

--- day1.py
+++ day1.py
@@ -35,12 +35,40 @@ if vote>=18:
 else:
     print("Cant")
 
-print("Printing 1 to 10")
+print("Printing 1 to 10 ")
 
-nom = 10
-for i in range (nom):
+for i in range (1,11):
+    print(i)
+print("Printing 10 to 1")
+
+for i in range (10,-1,-1):
     print(i)
 
+num1 = int(input("enter num u want table"))
 
-for i in range (10 -1 -1):
-    print(i)
+for i in range(1,11):
+    table = print(f"{num1}X{i}={num1*i}")
+
+nom = int(input("Enter num of n to sum"))
+
+sum = 0
+
+for i in range(1,nom+1):
+    sum+=i
+
+print(sum)
+
+
+c = int(input("Enter a number to count digits: "))
+
+count = 0
+
+# If the user enters 0, it still has 1 digit
+if c == 0:
+    count = 1
+else:
+    while c > 0:
+        c = c // 10  # This removes the last digit (e.g., 523 becomes 52)
+        count += 1   # Add 1 to the digit count
+
+print(f"The number of digits are {count}")
